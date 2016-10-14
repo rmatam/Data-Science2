6aec5d996a34fa34c7f6213b404588dc5e93f5fa
rmatam <sekhar.matam@gmail.com>
2016-10-15 00:36:36 +0530

    Add files via upload

---
+++ 03.Clustering/02.hierarchical clustering.R
@@ -0,0 +1,15 @@
+set.seed(1234)
+par(mar=c(0,0,0,0))
+x<-rnorm(12,mean = rep(1:3,each=4),sd = 0.2)
+y<-rnorm(12,mean = rep(1,2,1,each=4),sd = 0.2)
+plot(x,y,col="blue",pch= 19,cex=2)
+text(x+0.05,y+0.05,labels = as.character(1:12))
+
+#calculate the distance 
+dataframe=data.frame(x=x,y=y)
+dist(dataframe)
+distxy<-dist(dataframe)
+hclustering<-hclust(distxy)
+plot(hclustering)
+
+
